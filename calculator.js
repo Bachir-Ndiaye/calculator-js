25cf0641075c7dcccb6b71fea993aa4e79439711
Bachir-Ndiaye <amadou.n-diaye@hotmail.com>
2021-03-09 15:06:07 +0100

    added logic to my code

--- calculator.js
+++ calculator.js
@@ -1,4 +1,22 @@
-const myName = "Bachir";
-const myAge = 29;
-const myGender = "Male"
-console.log(myName + ' ' + myAge + ' ' + myGender);
+const numb_1 = prompt('Enter first number');
+const op     = prompt('Enter the correct operator')
+const numb_2 = prompt('Enter second number');
+
+
+switch (op) {
+    case '+':
+        console.log(parseInt(numb_1) + parseInt(numb_2));
+        break;
+    case '-':
+        console.log(parseInt(numb_1) - parseInt(numb_2));
+        break;
+    case '*':
+        console.log(parseInt(numb_1) / parseInt(numb_2));
+        break;
+    case '/':
+        console.log(parseInt(numb_1) % parseInt(numb_2));
+        break;
+    
+    default:
+        break;
+}
